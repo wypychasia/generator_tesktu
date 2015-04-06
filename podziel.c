8e4a7e20dfbebcca8700b9444b955c69f1d1d8da
Joanna Wypych <wypychj@len.iem.pw.edu.pl>
2015-04-06 21:20:46 +0200

    Poprawiona wersja podziel.c

--- podziel.c
+++ podziel.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include "podzial.h"
 
+lista_t wektor[1000];
+int dl_wektora =0;
+
 char* czytaj_slowo(FILE *tekst){
 	static char slowo[100];
 	char a;
@@ -18,33 +21,42 @@ char* czytaj_slowo(FILE *tekst){
 }
 void dopisz_ngram(char *wyraz){
 	int liczba_ngramow = 0;
-	lista_t *wektor;
-	int dl_wektora = 0;
 	int i;
 
 	for (i = 0; i < dl_wektora; i++){
 		if (strcmp(wyraz, wektor[i].wyraz) == 0){
-				break;
+			break;
 		}
 	}
 
 	strcpy(wektor[dl_wektora].wyraz, wyraz);
 	dl_wektora++;
 	liczba_ngramow++;
+	
 
 }
+void dopisz_slowo(char *sufiks, char *wyraz){
+	int i;
+
+	for (i = 0; i < dl_wektora; i++){
+		if (strcmp(wyraz, wektor[i].wyraz) == 0){
+			dodaj(wektor[i].next, sufiks);
+		}
+	}
+	printf("sufiks=%s\n",sufiks);
+}
 lista_t *dodaj(lista_t *l, char *slowo){
-	lista_t *aux;
+	lista_t *aux, *tmp;
 
 	aux = malloc(sizeof(lista_t));
 	strcpy(aux->wyraz, slowo);
 	aux->next = NULL;
 
-	if (l == NULL)
+	if (l == NULL){
 		l = aux;
-	else{
-		for (; l->next != NULL; l = l->next);
-		l->next = aux;
+	}else{
+		for (tmp = l; tmp->next != NULL; tmp = tmp->next);
+		tmp->next = aux;
 	}
 	return l;
 }
@@ -53,38 +65,78 @@ void podzial(FILE *tekst, int dltekstu,int ilosc_linii, int ngram){
 	int i,k;
 	char wyraz[5000];
 	char slowo[200];
+	char sufiks[200];
+
 
 	lista_t *l, *aux;
 	l = NULL;
 	wyraz[0] = 0;
 
-	for (i = 0; i < ngram; i++)
-		l = dodaj(l, czytaj_slowo(tekst));
+	for (i = 0; i < ngram; i++){
+		strcpy(slowo, czytaj_slowo(tekst));
+		l = dodaj(l, slowo);
+	}
 	for (aux = l; aux->next != NULL; aux = aux->next);
 	aux->next = l;
-		
+	
+	for(k=0; k<ngram; k++){
+	l=l->next;
+	}	
 	while (!feof(tekst)){
-
-		///
-
-		strcpy(slowo, czytaj_slowo(tekst));
-		strcpy(l->wyraz, slowo);
-		l = l->next;
 		wyraz[0] = 0;
 		for (aux = l, i=0; i<ngram; i++, aux= aux->next){
 			strcat(wyraz, aux->wyraz);
 			strcat(wyraz, " ");
 		}
-		dopisz_ngram(wyraz);
+                dopisz_ngram(wyraz);
+		strcpy(slowo, czytaj_slowo(tekst));
+		strcpy(l->wyraz, slowo);
+		l = l->next;
 
 	}
-}
-int main(int agrc, char **argv){
-	FILE *in = fopen(argv[1], "r");
-	int i;
+		wyraz[0]=0;
+                for (aux = l, i=0; i<ngram; i++, aux= aux->next){
+                        strcat(wyraz, aux->wyraz);
+                        strcat(wyraz, " ");
+                }
+		dopisz_ngram(wyraz);
+	rewind(tekst);
+	l=NULL;
+	for (i = 0; i < ngram+1; i++)
+		l = dodaj(l, czytaj_slowo(tekst));
+	for (aux = l; aux->next != NULL; aux = aux->next);
+	aux->next = l;
+	while (!feof(tekst)){
+		wyraz[0] = 0;
+		for (aux = l, i = 0; i<ngram; i++, aux = aux->next){
+			strcat(wyraz, aux->wyraz);
+			strcat(wyraz, " ");
+		}
+		printf("wyraz=%s\n",wyraz);
+		strcpy(sufiks, aux->wyraz);
+		dopisz_slowo(sufiks, wyraz);
+		strcpy(slowo, czytaj_slowo(tekst));
+		strcpy(l->wyraz, slowo);
+		l = l->next;
+        }
+  	 wyraz[0] = 0;
+         for (aux = l, i = 0; i<ngram; i++, aux = aux->next){
+            strcat(wyraz, aux->wyraz);
+            strcat(wyraz, " ");
+                }
+	 printf("wyraz=%s\n",wyraz);
+ strcpy(sufiks, aux->wyraz);
+                dopisz_slowo(sufiks, wyraz);
+	
 
-	for(i=0; i<atoi(argv[2]); i++)
-	printf("Słowo %d : %s\n", i,czytaj_slowo(in));
+
+}	
+
+int main(int argc, char **argv){
+	int i;
+	FILE *in = fopen(argv[1],"r");
+
+	podzial(in,16,1,4);	
 
 return 0;
 }
