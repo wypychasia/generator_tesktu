945edfb04f007949a1a35d62e83c2e73595c95b2
Joanna Wypych <wypychj@len.iem.pw.edu.pl>
2015-04-13 18:08:34 +0200

    Zmaina main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "statystyka.h"
 #include "generator.h"
+#include "podzial.h"
 
 void pomoc(){
 printf("Poczatkowo prosze o wpisanie flagi '-txt' z nazwa pliku, z ktorego będzie tworzony tekst wyjsciowy oraz flage '-plik' z nazwa pliku do ktorego wpisany zostanie tekst wyjsciowy. Następnie z flagą '-ngram' wybrac zadana dlugosc ngramu oraz z flagą '-dl_txt' wpisac dlugosc tekstu wejsciowego. By otrzymać informacje o statystyce należy wpisać flagę '-stat'.\n");
@@ -12,38 +13,52 @@ int main(int argc, char  **argv)
 {
   int dlugosc_tekstu = 0;
   int ngram = 0;
-  int i;
+  FILE *tekst, *plik;
 
-  if(strcmp(argv[1],"-pomoc")==0)
+  if(strcmp(argv[1],"-pomoc")==0){
 	pomoc();
+	return 0;
+  }
 
-  for(i=0; i<argc-1;i++){
-    if(strcmp(argv[i],"-txt")==0){
-      FILE *tekst = fopen(argv[i+1],"r");
-          if(tekst == NULL)
+  if(strcmp(argv[1],"-txt")==0){
+      FILE *tekst = fopen(argv[2],"r");
+          if(tekst == NULL){
 	    printf("Blad we wczytywaniu tekstu\n");
-    }
-    else if(srtcmp(argv[i],"-plik")==0){
-      FILE *plik = fopen(argv[i+1],"w");
-        if(plik == NULL)
+	      return -1;
+	  }	
+  }else
+	pomoc();
+  if(strcmp(argv[3],"-plik")==0){
+      FILE *plik = fopen(argv[4],"w");
+        if(plik == NULL){
      	  printf("Blad we wczytywaniu pliku wyjsiowego\n");  
-    }
-    else if(strcmp(argv[i],"-ngram")==0)
-      ngram=atoi(argv[i+1]);
+	     return -1;
+	}	
+    }else
+	pomoc();
+  if(strcmp(argv[5],"-ngram")==0){
+      ngram=atoi(argv[6]);
   	if(ngram == 0)
   	  ngram = 2;
-    else if(strcmp(argv[i],"-dl_txt")){
-      dlugosc_tekstu=atoi(argv[i+1]);
+    }
+  if(strcmp(argv[7],"-dl_txt")==0){
+      dlugosc_tekstu=atoi(argv[8]);
 	if(dlugosc_tekstu == 0)
 	  printf("Brak dlugosci tesktu - problemy ze statystyka\n");  
-    }
-    else if(strcmp(argv[i],"-stat")==0)
-      statystyka();
-  
-    else
+    } 
+  else{
       pomoc();
-}
-  if(tekst!=NULL && plik != NULL)
-     generowanie(tekst,ngram,plik);
+      return 0;
+  }
+  generowanie(tekst,ngram,plik);
+  printf("Wygenerowany tekst został zapisany do pliku\n");
+  if(strcmp(argv[9],"-stat")==0){
+
+      statystyka(plik);
+      return 0;
+  }
+
+  fclose(plik);
+  fclose(tekst);   
   return 0;
 }
