fd5ca4658c9437dd6c03152db3139610a6cf665c
Joanna Wypych <wypychj@len.iem.pw.edu.pl>
2015-04-14 01:05:36 +0200

    Prawie dokończona statystyka.c

--- statystyka.c
+++ statystyka.c
@@ -63,27 +63,70 @@ char *ngram(FILE *tekst){
 
 	return ngram;
 }
+/*char *slowo(FILE *tekst){
+	char tmp[100];
+	static char wyraz[100];
+	int n=0,i,nowa_dl=0,j, a=0,k,b=0;
+	int s=0, najwiekszy=0;
+	n=dl_txt(tekst);
+	tablica_t t[10000];
+	rewind(tekst);
+
+	strcpy(t[0].slowo,czytaj_slowo(tekst));
+	t[0].licznik++;
+	nowa_dl++;
+
+	for(i=1; i< n; i++){
+		strcpy(tmp,czytaj_slowo(tekst));
+		printf("nowa dl=%d\n", nowa_dl);
+	    for(j=0; j< nowa_dl; j++){
+		a=0;
+		a=t[j].licznik;
+		if(strcmp(t[j].slowo,tmp)==0){
+			t[j].licznik++;
+			b=0;
+			b=t[j].licznik;
+			printf("b=%d\n",b);
+	        }
+	        if(b > a){
+		   strcpy(t[i].slowo,tmp);
+		   t[i].licznik++;
+		   nowa_dl++;
+	    }
+	}
+	}
+	printf("nowa_dl=%d\n",nowa_dl);
+
+	for(k=0; k<nowa_dl; k++){
+	  s=t[k].licznik;
+	  if(s>najwiekszy){
+		najwiekszy=s;
+		strcpy(wyraz,t[k].slowo);
+	  }
+        }
+
+	
+	
+	rewind(tekst);
+	return wyraz;
+}*/
 void statystyka(FILE *plik, FILE *tekst){
 	int n=0;
 	double c=0;
 	char ng[500];
+	char s[100];
 	
 	n=dl_txt(tekst);
 	printf("Dlugosc tekstu wejsciowego = %d\n", n);
 
         strcpy(ng,ngram(tekst));
         printf("Najczęstszy ngram w tekście wejściowym to: ' %s' \n", ng);
-	
+/*	
+	strcpy(s,slowo(tekst));
+	printf("Najczęstsze słowo w tekście wejściowym to : %s \n", s);
+*/
 	c=czestotliwosc(plik);
 	printf("Częstotliwość podanego słowa wynosi %g\n",c);
 	
 }
-/*
-int main (int argc, char **argv){
 
-	FILE *in=fopen(argv[1],"r");
-	statystyka(in);
-
-  return 0;
-}
-*/
